b5245c68351c4b492a05d256b871e7b7406b198d
Loriano Storchi <loriano@storchi.org>
2018-04-11 15:24:00 +0200

    added basic verbosity option, added basi code to pass array from Python to Fortran

--- c_wrapper.c
+++ c_wrapper.c
@@ -1,23 +1,48 @@
 #include <string.h>
+#include <stdio.h>
 
-void __bertha_wrapper_MOD_bertha_init (char *, int);
+void __bertha_wrapper_MOD_bertha_init (char *, int *, int);
 void __bertha_wrapper_MOD_bertha_main (char *, char *, char *, char *, 
-    int, int, int, int);
+    double *, int, int, int, int);
+void __bertha_wrapper_MOD_bertha_finalize();
 
+extern int __spec_MOD_ndim;
 
-int bertha_init (char * filename)
+int bertha_init (char * filename, int verbosity)
 {
-  __bertha_wrapper_MOD_bertha_init(filename, strlen(filename));
+  __bertha_wrapper_MOD_bertha_init(filename, &verbosity, 
+      strlen(filename));
 
   return 0;
 }
 
 int bertha_main(char * fittcoefffname, char * vctfilename, 
-    char * ovapfilename, char * fittfname)
+    char * ovapfilename, char * fittfname, double * a)
 {
+  /*
+  int i;
+
+  for (i=0; i<10; i++)
+    printf("%f \n", a[i]);
+  */
+
   __bertha_wrapper_MOD_bertha_main(fittcoefffname, vctfilename, 
-        ovapfilename, fittfname, strlen(fittcoefffname), 
+        ovapfilename, fittfname, a, strlen(fittcoefffname), 
         strlen(vctfilename), strlen(ovapfilename), strlen(fittfname));
+
+  return 0;
+}
+
+int get_ndim ()
+{
+  int val = __spec_MOD_ndim;
+
+  return val;
+}
+
+int bertha_finalize ()
+{
+  __bertha_wrapper_MOD_bertha_finalize();
 
   return 0;
 }
--- pybertha.py
+++ pybertha.py
@@ -1,4 +1,5 @@
 import ctypes
+import numpy
 
 import os.path
 
@@ -21,7 +22,19 @@ in_ovapfilename = ctypes.c_char_p(ovapfilename)
 in_fnameinput = ctypes.c_char_p(fnameinput)
 in_fittfname = ctypes.c_char_p(fittfname)
 
-berthaw.bertha_init(in_fnameinput)
+verbosity = 0
+berthaw.bertha_init(in_fnameinput, ctypes.c_int(verbosity))
+ndim = berthaw.get_ndim()
+
+print "Verbosity: ", verbosity
+print "Matrix dimension: ", ndim
+
+a = numpy.zeros(ndim, dtype=numpy.double)
+for i in range(ndim):
+    a[i] = i
 
 berthaw.bertha_main(in_fittcoefffname, in_vctfilename, \
-        in_ovapfilename, in_fittfname)
+        in_ovapfilename, in_fittfname, \
+        ctypes.c_void_p(a.ctypes.data))
+
+berthaw.bertha_finalize()
