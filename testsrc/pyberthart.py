5d7c0c72fff39e734317603e12746760e4f99368
Matteo De Santis <matteo.des89@gmail.com>
2018-07-05 17:15:53 +0200

    backup propagator

---
+++ testsrc/pyberthart.py
@@ -0,0 +1,114 @@
+import ctypes
+import numpy
+import sys
+import re
+import os.path
+
+import rtutil
+
+from numpy.linalg import eigvalsh
+from scipy.linalg import eigh
+
+sys.path.insert(0, '../src/')
+import berthamod
+
+bertha = berthamod.pybertha("../../lib/bertha_wrapper.so")
+
+fittcoefffname = "fitcoeff.txt"
+vctfilename = "vct.txt" 
+ovapfilename = "ovap.txt"
+fnameinput = "input.inp"
+fittfname = "fitt2.inp"
+
+verbosity = -1
+dumpfiles = 0
+
+
+bertha.set_fittcoefffname(fittcoefffname)
+bertha.set_ovapfilename(ovapfilename)
+bertha.set_vctfilename(vctfilename)
+bertha.set_fnameinput(fnameinput)
+bertha.set_fittfname(fittfname)
+
+bertha.set_verbosity(verbosity)
+bertha.set_dumpfiles(dumpfiles)
+
+bertha.init()
+
+ndim = bertha.get_ndim()
+nshift = bertha.get_nshift()
+nocc = bertha.get_nocc()
+sfact = bertha.get_sfact()
+nopen = bertha.get_nopen()
+
+print "Verbosity       : ", verbosity
+print "Dumpfiles       : ", dumpfiles
+print ""
+print "Matrix dimension: ", ndim
+print "            nocc: ", nocc
+print "          nshift: ", nshift
+print "           nopen: ", nopen
+print "     level shift: ", sfact
+print ""
+
+ovapm, eigem, fockm, eigen = bertha.run()
+print "fock[0,10] = ", fockm[0,10]
+if (fockm is None) or (eigen is None) or (fockm is None) \
+        or (eigen is None):
+    print "Error in bertha run"
+    exit(-1)
+
+print ""
+print "Final results "
+for i in range(nocc+nopen):
+    print "eigenvalue %5d %20.8f"%(i+1, eigen[i+nshift]-sfact)
+    
+print "      lumo       %20.8f"%(eigen[i+nshift+1])
+
+erep = bertha.get_erep()
+etotal = bertha.get_etotal()
+
+print ""
+print "total electronic energy  = %20.8f"%(etotal-(sfact*nocc))
+print "nuclear repulsion energy = %20.8f"%(erep)
+print "total energy             = %20.8f"%(etotal+erep-(sfact*nocc))
+
+bertha.realtime_init()
+
+D_0=numpy.zeros((ndim,ndim),dtype=numpy.complex128)
+for num in range(nocc):
+    D_0[num,num]=1.0+0.0j
+dt=0.1
+#containers
+ene_list = []
+dip_list = []
+imp_list = []
+C = eigem
+#C_inv used to backtransform D(AO)
+C_inv = numpy.linalg.inv(C)
+#build density in ao basis
+occeigv = numpy.zeros((ndim,nocc), dtype=numpy.complex128)
+
+iocc = 0
+
+for i in range(ndim):
+    if i >= nshift and iocc < nocc:
+        for j in range(ndim):
+            occeigv[j, iocc] = eigem[j, i]
+        iocc = iocc + 1
+
+Da=numpy.matmul(occeigv,numpy.conjugate(occeigv.transpose()))
+
+direction = 2
+normalise = 1
+
+dipz_mat = bertha.get_realtime_dipolematrix(direction, normalise)
+
+print numpy.matmul(C,C_inv)
+
+fock_mid_init = rtutil.mo_fock_mid_forwd_eval(bertha,Da,fockm,0,numpy.float_(dt),\
+	dipz_mat,C,C_inv,ovapm,ndim)
+
+
+bertha.finalize()
+
---
+++ testsrc/rtutil.py
@@ -0,0 +1,79 @@
+import numpy 
+import sys
+
+def exp_opmat(mat,dt):
+#first find eigenvector of a  matrix defined as -iF*dt, F beig hermitian
+# and take the exponential of the diagonal
+#for our purpose mat=-iF, F being hermitian
+    w,v=numpy.linalg.eig(-1.j*dt*mat)
+    diag=numpy.exp(w)
+#build the diagonal matrix
+# use numpy.diagflat(w)
+#   dmat=numpy.zeros(mat.shape,dtype=float)
+#   for num in range(diag.shape[0]):
+#       dmat[num,num]=diag[num]
+    dmat=numpy.diagflat(diag)
+# for a general matrix Diag = M^(-1) A M
+# M is v 
+    v_i=numpy.linalg.inv(v)
+#transform back
+#matmul introduced in numpy 1.10 is preferred with respect numpy.dot 
+    tmp=numpy.matmul(dmat,v_i)
+#for real matrices conjugation=transposition
+    mat_exp=numpy.matmul(v,tmp)
+    return mat_exp
+#################################################
+
+def kick(Fmax,t):
+    func = 0.0
+
+    if t >0:
+      func=0.0
+    elif (t == 0.0):
+      func = Fmax
+    else:
+     print('t out of range')
+
+    return func
+
+#################################################
+
+def mo_fock_mid_forwd_eval(bertha,D_ti,fock_mid_ti_backwd,i,delta_t,dipole_z,C,C_inv,S,ndim):
+
+   fock_inter=numpy.zeros((ndim,ndim),dtype=numpy.complex128)   
+   
+   #D_ti is in AO basis 
+   #transform in the MO ref basis
+   Dp_ti= numpy.matmul(C_inv,numpy.matmul(D_ti,numpy.conjugate(C_inv.T)))
+   k=1
+   t_arg=numpy.float_(i)*numpy.float_(delta_t)
+   fockmtx = bertha.get_realtime_fock(D_ti.T)
+   fock_ti_ao=fockmtx+0.0*kick(0.00002,t_arg)
+   #dipole matrix null for test
+   dens_test=numpy.zeros((ndim,ndim),dtype=numpy.complex128)
+   fock_guess = 2.00*fock_ti_ao - fock_mid_ti_backwd
+   while True:
+        fockp_guess=numpy.matmul(numpy.conjugate(C.T),numpy.matmul(fock_guess,C))
+        u=exp_opmat(fockp_guess,delta_t)
+        tmpd=numpy.matmul(Dp_ti,numpy.conjugate(u.T))
+        Dp_ti_dt=numpy.matmul(u,tmpd)
+    #backtrasform Dp_ti_dt
+        D_ti_dt=numpy.matmul(C,numpy.matmul(Dp_ti_dt,numpy.conjugate(C.T)))
+    #build the correspondig Fock , fock_ti+dt
+        
+        fock_ti_dt_ao=bertha.get_realtime_fock(D_ti_dt.T)+0.0*kick(0.00002,t_arg+delta_t)
+        fock_inter= 0.5*fock_ti_ao + 0.5*fock_ti_dt_ao
+    #update fock_guess
+        fock_guess=numpy.copy(fock_inter)
+        if k >1:
+        #test on the norm: compare the density at current step and previous step
+        #calc frobenius of the difference D_ti_dt_mo_new-D_ti_dt_mo
+            diff=D_ti_dt-dens_test
+            norm_f=numpy.linalg.norm(diff,'fro')
+            print norm_f
+            if norm_f<(1e-6):
+                tr_dt=numpy.trace(numpy.matmul(S,D_ti_dt))
+                break
+        dens_test=numpy.copy(D_ti_dt)
+        k+=1
+   return fock_inter
