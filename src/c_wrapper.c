9e54fa54066ac55e969f8495a73cf83a773489b4
Loriano Storchi <loriano@storchi.org>
2018-07-27 12:05:51 +0200

    started renaming to be used by the PRECOMPILER

--- src/c_wrapper.c
+++ src/c_wrapper.c
@@ -1,54 +1,80 @@
 #include <string.h>
 #include <stdio.h>
 
+#ifdef USEINTELCMP
+
+#else
+
 void __bertha_wrapper_MOD_bertha_init (char *, int *, int *, int);
 void __bertha_wrapper_MOD_bertha_realtime_init ();
 void __bertha_wrapper_MOD_bertha_main (char *, char *, char *, char *, 
     double *, double *, double *, double *, int, int, int, int);
 void __bertha_wrapper_MOD_bertha_finalize();
 void __bertha_wrapper_MOD_bertha_realtime_finalize();
-void  __bertha_wrapper_MOD_bertha_realtime_dipolematrix(int *, 
+void __bertha_wrapper_MOD_bertha_realtime_dipolematrix(int *, 
       int *, double *);
-void  __bertha_wrapper_MOD_bertha_realtime_fock (double *, double *);
+void __bertha_wrapper_MOD_bertha_realtime_fock (double *, double *);
 
-extern int __spec_MOD_ndim, __spec_MOD_nshift, __spec_MOD_nocc, 
+extern int _spec_MOD_ndim, __spec_MOD_nshift, __spec_MOD_nocc, 
        __opensh_MOD_nopen;
 extern double __shiftr_MOD_sfact, __energy_MOD_etotal, 
        __bertha_wrapper_MOD_erep;
+
+#define f_bertha_init __bertha_wrapper_MOD_bertha_init
+#define f_bertha_realtime_init __bertha_wrapper_MOD_bertha_realtime_init
+#define f_bertha_main __bertha_wrapper_MOD_bertha_main
+#define f_bertha_finalize __bertha_wrapper_MOD_bertha_finalize
+#define f_bertha_realtime_finalize __bertha_wrapper_MOD_bertha_realtime_finalize
+#define f__bertha_realtime_dipolematrix __bertha_wrapper_MOD_bertha_realtime_dipolematrix
+#define f__bertha_realtime_fock __bertha_wrapper_MOD_bertha_realtime_fock
+
+#define f_ndim __spec_MOD_ndim
+#define f_nshift __spec_MOD_nshift
+#define f_nocc __spec_MOD_nocc
+#define f_nopen __opensh_MOD_nopen
+
+#define f_sfact __shiftr_MOD_sfact
+/*
+ __energy_MOD_etotal      
+ __bertha_wrapper_MOD_erep
+*/
+
+#define 
+#endif
 
 // DATA METHODS
 
 int get_ndim ()
 {
-  int val = __spec_MOD_ndim;
+  int val = f_ndim;
 
   return val;
 }
 
 int get_nshift ()
 {
-  int val = __spec_MOD_nshift;
+  int val = f_nshift;
 
   return val;
 }
 
 int get_nopen ()
 {
-  int val = __opensh_MOD_nopen;
+  int val = f_nopen;
 
   return val;
 }
 
 int get_nocc ()
 {
-  int val = __spec_MOD_nocc;
+  int val = f_nocc;
 
   return val;
 }
 
 double get_sfact ()
 {
-  double val = __shiftr_MOD_sfact;
+  double val = f_sfact;
 
   return val;
 }
@@ -71,7 +97,7 @@ double get_etotal ()
 
 int init (char * filename, int verbosity, int dumpfiles)
 {
-  __bertha_wrapper_MOD_bertha_init(filename, &verbosity, 
+  f_bertha_init(filename, &verbosity, 
       &dumpfiles, strlen(filename));
 
   return 0;
@@ -79,7 +105,7 @@ int init (char * filename, int verbosity, int dumpfiles)
 
 int realtime_fock (double * dens_ptr, double * fock_ptr)
 {
-  __bertha_wrapper_MOD_bertha_realtime_fock (dens_ptr, fock_ptr);
+  f_bertha_realtime_fock (dens_ptr, fock_ptr);
 
   return 0;
 }
@@ -87,7 +113,7 @@ int realtime_fock (double * dens_ptr, double * fock_ptr)
 int realtime_dipolematrix (int direction, int norm, 
     double * vext_ptr)
 {
-  __bertha_wrapper_MOD_bertha_realtime_dipolematrix(&direction, 
+  f_bertha_realtime_dipolematrix(&direction, 
       &norm, vext_ptr);
 
   return 0;
@@ -95,7 +121,7 @@ int realtime_dipolematrix (int direction, int norm,
 
 int realtime_init ()
 {
-  __bertha_wrapper_MOD_bertha_realtime_init();
+  f_bertha_realtime_init();
 
   return 0;
 }
@@ -110,7 +136,7 @@ int mainrun(char * fittcoefffname, char * vctfilename,
     printf("%f \n", a[i]);
   */
 
-  __bertha_wrapper_MOD_bertha_main(fittcoefffname, vctfilename, 
+  f_bertha_main(fittcoefffname, vctfilename, 
         ovapfilename, fittfname, eigen, ovapin, eigenv, fockin,
         strlen(fittcoefffname), strlen(vctfilename), strlen(ovapfilename), 
         strlen(fittfname));
@@ -152,14 +178,14 @@ int mainrun(char * fittcoefffname, char * vctfilename,
 
 int finalize ()
 {
-  __bertha_wrapper_MOD_bertha_finalize();
+  f_bertha_finalize();
 
   return 0;
 }
 
 int realtime_finalize ()
 {
-  __bertha_wrapper_MOD_bertha_realtime_finalize();
+  f_bertha_realtime_finalize();
 
   return 0;
 }
