c9963450c75f30ab0acedb72ee1c99704527f64b
Loriano Storchi <loriano@storchi.org>
2021-06-18 17:17:39 +0200

    fix

--- full_eda_nocv.py
+++ full_eda_nocv.py
@@ -9,9 +9,10 @@ import os
 def execute(cmd):
 
     process = subprocess.Popen(cmd, stdout=subprocess.PIPE)
+
     while True:
         output = process.stdout.readline()
-        if output == '' and process.poll() is not None:
+        if process.poll() is not None:
             break
         if output:
             print(output.strip().decode("utf-8"), flush=True)
