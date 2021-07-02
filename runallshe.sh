74237cdcc45154ed80565d0b655d23160088234d
Loriano Storchi <loriano@storchi.org>
2021-07-02 13:08:20 +0200

    full SHE list

--- runallshe.sh
+++ runallshe.sh
@@ -25,7 +25,6 @@ export FITSET="Au:b20,Hg:hg"
 
 callfunctions
 
-
 export SYSYEMNAME="AuCn+"
 export ATOMB="Cn"
 export ATOMA="Au+"
@@ -33,4 +32,40 @@ export BASISSET="Au:dyall_vtz,Cn:dyall_vtz"
 export FITSET="Au:b20,Cn:cn"
 
 callfunctions
+
+export SYSYEMNAME="AuPb+"
+export ATOMB="Pb"
+export ATOMA="Au+"
+export BASISSET="Au:dyall_vtz,Pb:dyall_vtz"
+export FITSET="Au:b20,Pb:pb"
+
+callfunctions
+
+
+export SYSYEMNAME="AuFl+"
+export ATOMB="Fl"
+export ATOMA="Au+"
+export BASISSET="Au:dyall_vtz,Fl:dyall_vtz"
+export FITSET="Au:b20,Fl:fl"
+
+callfunctions
+
+
+export SYSYEMNAME="AuRn+"
+export ATOMB="Rn"
+export ATOMA="Au+"
+export BASISSET="Au:dyall_vtz,Rn:dyall_vtz"
+export FITSET="Au:b20,Rn:rn"
+
+callfunctions
+
+
+export SYSYEMNAME="AuOg+"
+export ATOMB="Og"
+export ATOMA="Au+"
+export BASISSET="Au:dyall_vtz,Og:dyall_vtz"
+export FITSET="Au:b20,Og:og"
+
+callfunctions
+
 
