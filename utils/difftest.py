8f2a6ad0966e7895698ab84185e0a51d8cc0aab7
Loriano Storchi <loriano@storchi.org>
2021-09-09 11:39:15 +0200

    minimal util

---
+++ utils/difftest.py
@@ -0,0 +1,20 @@
+import sys
+import numpy
+
+fp = open(sys.argv[1], "r")
+
+mx = 0.0e0 
+
+for l in fp:
+    sl = l.split()
+
+    v1 = numpy.float64(sl[0]) 
+    v2 = numpy.float64(sl[1]) 
+
+    print(v2-v1)
+
+    if (v2-v1) > max:
+        max = v2-v1
+
+
+print("Maxval: ", max)
