101999f8609bf6e5efffd3f54c5c82c64264d074
Loriano Storchi <loriano@storchi.org>
2021-10-06 11:59:51 +0200

    backup

--- utils/4Dplots_pot.py
+++ utils/4Dplots_pot.py
@@ -1,5 +1,7 @@
+from os import path
 import numpy as np
 from mayavi import mlab
+from scipy.interpolate import RegularGridInterpolator
 
 import sys
 
@@ -34,8 +36,16 @@ for line in fp:
     else:
         print("ERROR at line ", line)
 
+print(len(set(xs)), len(xs))
 
-x, y, z = np.ogrid[-10:10:20j, -10:10:20j, -10:10:20j]
+print(len(set(ys)), len(ys))
+
+print(len(set(zs)), len(zs))
+
+x, y, z = np.ogrid[min(xs):max(xs):100j, \
+    min(ys):max(ys):100j, \
+        min(zs):max(zs):100j]
+
 s = np.sin(x*y*z)/(x*y*z)
 
 print(s.shape)
@@ -50,13 +60,41 @@ print(min(xs), max(xs))
 
 mlab.pipeline.image_plane_widget(mlab.pipeline.scalar_field(s),
                             plane_orientation='x_axes',
-                            slice_index=10,
+                            slice_index=50,
                         )
 mlab.pipeline.image_plane_widget(mlab.pipeline.scalar_field(s),
                             plane_orientation='y_axes',
-                            slice_index=10,
+                            slice_index=50,
                         )
 mlab.outline()
-mlab.volume_slice(s, plane_orientation='x_axes', slice_index=10)
+mlab.volume_slice(s, plane_orientation='x_axes', slice_index=30)
 
 mlab.show()
+
+"""
+import numpy as np
+from scipy.interpolate import RegularGridInterpolator
+
+def f(x, y, z):
+  return 2 * x**3 + 3 * y**2 - z
+
+x = np.linspace(1, 4, 11)
+y = np.linspace(4, 7, 22)
+
+z = np.linspace(7, 9, 33)
+xg, yg ,zg = np.meshgrid(x, y, z, indexing='ij', sparse=True)
+
+print(xg.shape)
+print(yg.shape)
+print(zg.shape)
+
+data = f(xg, yg, zg)
+
+print(data.shape)
+
+my_interpolating_function = RegularGridInterpolator((x, y, z), data)
+pts = np.array([[2.1, 6.2, 8.3], [3.3, 5.2, 7.1]])
+
+my_interpolating_function(pts)
+
+"""
