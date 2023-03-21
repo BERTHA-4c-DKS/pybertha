9a777b1d18f6ebed6374d3e13a7932c7e8c2cfad
Loriano Storchi <loriano@storchi.org>
2023-03-21 14:04:46 +0100

    minor

--- pyberthart/pade.sh
+++ pyberthart/pade.sh
@@ -16,33 +16,26 @@ export LD_LIBRARY_PATH=${PWD%/*}"/lib":$LD_LIBRARY_PATH
 # limit must be < number of point in the dipole file
 
 export n=1
-for gamma in  1.0e-2 2.0e-2 1.0e-3 1.5e-3 2.0e-3
+for dump in gauss exp hann
 do
-   for dw in  0.02 0.002 0.01 0.1 
-   do
-	echo $gamma $dw
-	python /home/redo/BERTHA/Exanalysis/misc/pade/pade_transform.py --limit 25000 --gamma $gamma -f ./omp.mklserial.quad.2/dipole.txt --dw $dw --fmin 2.5 --frequency 6.5 -o zz_w.txt > pade.txt
-	sed "s/dw/$dw/" plot.p > tmp
-	sed "s/gamma/$gamma/" tmp > plot.p$n
-	gnuplot < plot.p$n
-	mv 1.pdf  omp_mklserial_quad_2_"$n".pdf
-	rm -f  plot.p$n tmp
-	n=$((n+1))
-   done
-done
-
-export n=1
-for gamma in  1.0e-2 2.0e-2 1.0e-3 1.5e-3 2.0e-3
-do
-   for dw in  0.02 0.002 0.01 0.1 
-   do
-	echo $gamma $dw
-	python /home/redo/BERTHA/Exanalysis/misc/pade/pade_transform.py --limit 25000 --gamma $gamma -f ./omp.mklparallel.quad.2/dipole.txt --dw $dw --fmin 2.5 --frequency 6.5 -o zz_w.txt > pade.txt
-	sed "s/dw/$dw/" plot.p > tmp
-	sed "s/gamma/$gamma/" tmp > plot.p$n
-	gnuplot < plot.p$n
-	mv 1.pdf  omp_mklserial_quad_2_"$n".pdf
-	rm -f  plot.p$n tmp
-	n=$((n+1))
-   done
+  for gamma in  1.0e-2 2.0e-2 1.0e-3 1.5e-3 2.0e-3
+  do
+    for dw in  0.02 0.002 0.01 0.1 
+    do
+      echo $gamma $dw $dump
+      python /home/redo/BERTHA/Exanalysis/misc/pade/pade_transform.py --damping $dump --limit 25000 --gamma $gamma \
+	      -f ./omp.mklserial.quad.2/dipole.txt --dw $dw --fmin 2.5 --frequency 6.5 -o zz_w.txt > pade.txt
+      sed "s/dw/$dw/" plot.p > tmp
+      sed "s/dump/$dump/" tmp > tmp1
+      sed "s/gamma/$gamma/" tmp1 > plot.p$n
+      gnuplot < plot.p$n
+      mv 1.pdf  omp_mklserial_quad_2_"$n".pdf
+      python /home/redo/BERTHA/Exanalysis/misc/pade/pade_transform.py --damping $dump --limit 25000 --gamma $gamma \
+	      -f ./omp.mklparallel.quad.2/dipole.txt --dw $dw --fmin 2.5 --frequency 6.5 -o zz_w.txt > pade.txt
+      gnuplot < plot.p$n
+      mv 1.pdf  omp_mklparallel_quad_2_"$n".pdf
+      rm -f  plot.p$n tmp tmp1
+      n=$((n+1))
+    done
+  done
 done
--- pyberthart/plot.p
+++ pyberthart/plot.p
@@ -1,4 +1,4 @@
 set terminal pdf 
 set output "1.pdf"
-plot "zz_w.txt"  u ($1*27.2114):($1*$3) w l lw 2 ti "DW=dw GAMMA=gamma"
+plot "zz_w.txt"  u ($1*27.2114):($1*$3) w l lw 2 ti "DW=dw GAMMA=gamma DUMPING=dump"
 
